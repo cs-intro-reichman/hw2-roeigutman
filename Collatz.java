854b8ba849420b9bb7292342b8c7cb73cccf67e1
roei gutman <git config --global user.email david.german@post.runi.ac.il>
2025-11-11 19:25:39 +0200

    Completed assignment

--- Collatz.java
+++ Collatz.java
@@ -1,42 +1,29 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	  
-int N = Integer.parseInt(args[0]);
- String mode = args[1];
-  
- for( int i = 1; i <= N; i++) {
-  
-	int seed = i;
-	int count = 1;
-	if(mode.equals("v")){
+	    
+		int n = Integer.parseInt(args[0]);
+		String mode = args[1];
+		
+		if (mode.equals("v")){
+	        for (int i = 1; i <= n; i++) {
+			    int seed = i;
+			    int count = 1;
+
 				System.out.print(seed +" ");
-	}
-	while ( seed != 1 || count == 1 ) {
-
-	if( seed % 2 == 0) {
-       seed = seed / 2;
-       
-   } else { seed = seed * 3 + 1;
-
-	}
-     if(mode.equals("v")){
-					System.out.print(seed +" ");
-	}
-      
-	  count++;
-	
-	}
-    
-	if(mode.equals("v")){	
-				System.out.println("(" + count + ")");
-	}		
-      
-   }
-
-   System.out.println("Every one of the first " + N + " hailstone sequences reached 1.");
-      
-
+			    while (seed != 1 || count == 1) {
+				    if(seed % 2 == 0) {
+					    seed = seed / 2;
+				    } else {seed = seed * 3 + 1;}
+				    System.out.print(seed +" ");
+				    count++;
+			    }
+			    System.out.println("(" + count + ")");
+	        }
+		}
+		
+		System.out.println("Every one of the first " + n + " hailstone sequences reached 1.");
+	    
 	}
 
 }
